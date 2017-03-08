fda2f786c3e358b3f393a27b6df99fa42db433c7
Barry Closser <barry.closser@gmail.com>
2017-03-07 20:37:06 -0600

    Import Bootstrap via CDN Basic mock of requirements

--- src/js/client.js
+++ src/js/client.js
@@ -19,13 +19,41 @@ class App extends React.Component {
 			.then(response => response.json())
 			.then(json => {
 				this.setState({members: json, isLoading: false});
+				console.log("members loaded");
 			});
 	}
 
 	render() {
     return (
       <div>
-        <h1>UX Members</h1>
+        <h1>UX Society Members</h1>
+        <input type="text" className="form-control" placeholder="search"></input>
+        <table className="table table-striped">
+        	<thead>
+        		<tr>
+	        		<th>Name</th>
+	        		<th>Occupation</th>
+	        		<th>Company</th>
+        		</tr>
+        	</thead>
+        	<tbody>
+        		<tr>
+	        		<td>Name</td>
+	        		<td>Occupation</td>
+	        		<td>Company</td>
+        		</tr>
+        		<tr>
+	        		<td>Name</td>
+	        		<td>Occupation</td>
+	        		<td>Company</td>
+        		</tr>
+        	</tbody>
+        </table>
+        <div id="memberDetails">
+        	<h2>NAME</h2>
+        	<img />
+        	<p>QUOTE</p>
+        </div>
       </div>
     );
   }
